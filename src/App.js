b278b7b9caff8614302e26af464fc85578e27c87
hana <rlagksk8428@gmail.com>
2022-07-21 23:47:24 +0900

    feat: set up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import {Routes, Route} from "react-router-dom"
+import About from "./components/About";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Lab from "./components/Lab";
+import Project from "./components/Project";
+import Writing from "./components/Writing";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Routes>
+        <Route path="/writing" element={<Writing/>} />
+        <Route path="/lab" element={<Lab/>} />
+        <Route path="/project" element={<Project/>} />
+        <Route path="/about" element={<About/>} />
+        <Route path="/" element={<Home/>} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Writing.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Writing() {
+  return (
+    <div>Writing</div>
+  )
+}
