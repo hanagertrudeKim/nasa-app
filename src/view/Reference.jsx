a46e8f791fad8e258fb9bad99d60a09a61a1e166
hana <rlagksk8428@gmail.com>
2022-07-27 04:20:46 +0900

    feat : test reference api

--- src/view/Reference.jsx
+++ src/view/Reference.jsx
@@ -1,6 +1,21 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import API_KEY from '../api/nasaApi';
 
-export default function Reference() {
+
+export default function Asteroid() {
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+
+    axios.get(`ttps://api.nasa.gov/techport/api/projects/17792?api_key=${API_KEY}`)
+    .then((res)=> {
+      setData(res.data);
+      console.log(res);
+    });
+  },[])
+
   return (
     <div>Reference</div>
   )
