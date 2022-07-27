cb867fb368941f9af8cae466583d0f7c84c457ee
hana <rlagksk8428@gmail.com>
2022-07-27 17:44:00 +0900

    fix : APOD axios 문제 해결

--- src/view/Home/APOD.jsx
+++ src/view/Home/APOD.jsx
@@ -6,13 +6,11 @@ import * as S from './APOD.style'
 
 export default function APOD() {
   const [data, setData] = useState();
-  const [mediaType, setMediaType] = useState();
 
   useEffect(() => {
     axios.get(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`)
     .then((res)=> {
       setData(res.data);
-      setMediaType(res.data.media_type);
       console.log(res.data);
     })
   },[])
@@ -21,11 +19,11 @@ export default function APOD() {
   const PhotoAPOD = () => {
     return (
       <S.PhotoWrap>
-        <S.Photo src={data.hdurl} />
+        <S.Photo src={data?.hdurl} />
         <S.ExplainWrap>
-        <S.Title>Title : {data.title} </S.Title><br/>
-        <S.Explain>date: {data.date} <br/> <br/>
-        explanation : {data.explanation} <br/></S.Explain>
+        <S.Title>Title : {data?.title} </S.Title><br/>
+        <S.Explain>date: {data?.date} <br/> <br/>
+        explanation : {data?.explanation} <br/></S.Explain>
         </S.ExplainWrap>
       </S.PhotoWrap>
     )
@@ -34,19 +32,18 @@ export default function APOD() {
   const VideoAPOD = () => {
     return (
       <S.VideoWrap>
-        <S.Title>Title : {data.title} </S.Title><br/>
-        <S.Explain>date: {data.date} <br/> <br/>
-        explanation : {data.explanation} <br/></S.Explain>
-        <S.VideoLink><a href={data.url}>Go to the video</a></S.VideoLink>
+        <S.Title>Title : {data?.title} </S.Title><br/>
+        <S.Explain>date: {data?.date} <br/> <br/>
+        explanation : {data?.explanation} <br/></S.Explain>
+        <S.VideoLink><a href={data?.url}>Go to the video</a></S.VideoLink>
       </S.VideoWrap>
     )
   }
 
-
   return (
     <S.APODWrap>
       <S.APODTitle> APOD  - Astronomy Picture Of the Day</S.APODTitle> 
-      {mediaType === "video" ? <VideoAPOD /> : <PhotoAPOD />}
+      {data?.mediaType === "video" ? <VideoAPOD /> : <PhotoAPOD />}
     </S.APODWrap>
   )
 }
