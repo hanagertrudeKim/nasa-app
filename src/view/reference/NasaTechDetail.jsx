0ceace1c1d8df97239b4f68b11a6de7aa1cdcf4a
hana <rlagksk8428@gmail.com>
2022-08-06 11:39:46 +0900

    fix : remove duplicate api request in nasa tech detail page

--- src/view/reference/NasaTechDetail.jsx
+++ src/view/reference/NasaTechDetail.jsx
@@ -1,32 +1,26 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react';
 import axios from 'axios';
 import API_KEY from '../../api/nasaApi';
-import * as S from './nasaTechDetail.styled';
+import * as S from './nasaTech.styled';
+import { useLocation } from 'react-router-dom';
 
 export default function NasaTechDetail() {
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-
-    axios.get(`https://api.nasa.gov/techtransfer/patent/?engine&api_key=${API_KEY}`)
-    .then((res)=> {
-      (res.data?.results).map((res) => {
-      if(document.location.href.split('/').reverse()[0] === res[0]){
-        setData(res)
-      }
-      return res
-    })
-      console.log(res.data?.results);
-    }).catch((err) => console.log(err));
-  },[]);
+  const location = useLocation();
+  const data = location.state;
 
   return (
     <S.Warp>
-      <S.TitleWrap>
+      <S.TitleWrap to={`/reference/nasaTech/${data[0]}`} state={data} key={data[0]}>
+        <S.FrontImg src={data[10]} alt="Nasa Tech" />
         <S.Title>
-        {data}
+          {data[2].indexOf('<') > 0
+            ? data[2].substr(0, data[2].indexOf('<')) +
+              data[2].substr(data[2].indexOf('>') + 1, data[2].indexOf('<') - 3)
+            : data[2]}
         </S.Title>
+        <S.Number>{data[1]}</S.Number>
       </S.TitleWrap>
+      <div dangerouslySetInnerHTML={{ __html: data[3] }}></div>
     </S.Warp>
-  )
+  );
 }
--- src/view/reference/nasaTech.styled.js
+++ src/view/reference/nasaTech.styled.js
@@ -1,5 +1,5 @@
-import styled from "styled-components";
-import { Link } from "react-router-dom";
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 export const Wrap = styled.div`
   color: white;
@@ -13,39 +13,43 @@ export const Wrap = styled.div`
   place-items: center;
   place-self: center;
   justify-content: space-around;
-`
+`;
 
 export const TitleWrap = styled(Link)`
-  border: 1px solid #9BDAF1;
+  border: 1px solid #9bdaf1;
   margin-bottom: 20px;
   width: 260px;
   height: 400px;
   text-decoration: none;
-  &:focus, &:hover, &:visited, &:link, &:active {
+  &:focus,
+  &:hover,
+  &:visited,
+  &:link,
+  &:active {
     text-decoration: none;
   }
   &:-webkit-any-link {
     color: inherit;
     cursor: pointer;
-  &:hover{
-    background-color: #212121;
+    &:hover {
+      background-color: #212121;
+    }
   }
-}
-`
+`;
 export const FrontImg = styled.img`
   width: 260px;
   height: 230px;
-`
+`;
 
 export const Title = styled.div`
   font-family: sans-serif;
   letter-spacing: 1px;
   font-size: 15px;
   font-weight: 800;
-  color: #9BDAF1;
+  color: #9bdaf1;
   padding: 5px;
   background-color: inherit;
-`
+`;
 
 export const Number = styled.div`
   font-family: 'Courier New', Courier, monospace;
@@ -53,4 +57,17 @@ export const Number = styled.div`
   font-size: 12px;
   padding: 5px;
   background-color: inherit;
-`
+`;
+
+export const Warp = styled.div`
+  display: flex;
+  flex-direction: column;
+  place-content: center;
+  place-items: center;
+  place-self: center;
+  color: white;
+  padding: 100px;
+  .highlight {
+    color: orange;
+  }
+`;
