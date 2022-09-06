6c4d0fbf8c7966d06fe3e56e89970df504265f60
hana <rlagksk8428@gmail.com>
2022-09-06 15:24:44 +0900

    feat : add nasatech react-query

--- src/view/reference/NasaTech.jsx
+++ src/view/reference/NasaTech.jsx
@@ -1,30 +1,23 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import * as S from './nasaTech.styled';
 import { getNasaTech } from '../../api/nasaApi';
 import TopBtn from '../../components/TopBtn';
+import { useQuery } from 'react-query';
 
 export default function NasaTech() {
-  const [data, setData] = useState([]);
-  const [arr, setArr] = useState([]);
+  const { data: res } = useQuery('nasatech', getNasaTech);
 
   useEffect(() => {
-    getNasaTech()
-      .then((res) => {
-        setData(res.data);
-        setArr(res.data?.results);
-        console.log(res.data?.results);
-      })
-      .catch((err) => console.log(err));
     window.scrollTo(0, 0);
   }, []);
 
   return (
     <S.Wrap>
-      {arr.map((data) => {
+      {res?.data?.results.map((data, index) => {
         return (
           <>
             <TopBtn />
-            <S.TitleWrap to={`/reference/nasaTech/${data[0]}`} state={data} key={data[0]}>
+            <S.TitleWrap to={`/reference/nasaTech/${index}`} key={data[0]}>
               <S.FrontImg src={data[10]} alt="Nasa Tech" />
               <S.Title dangerouslySetInnerHTML={{ __html: data[2] }}></S.Title>
               <S.Number>{data[1]}</S.Number>
--- src/view/reference/NasaTechDetail.jsx
+++ src/view/reference/NasaTechDetail.jsx
@@ -1,28 +1,36 @@
 import React from 'react';
 import * as S from './nasaTechDetail.styled';
-import { useLocation } from 'react-router-dom';
+import { getNasaTech } from '../../api/nasaApi';
 import { useEffect } from 'react';
+import { useQuery } from 'react-query';
+import { useParams } from 'react-router-dom';
 
 export default function NasaTechDetail() {
-  const location = useLocation();
-  const data = location.state;
-  console.log(data);
+  const { data: res } = useQuery('nasatech', getNasaTech);
+  const { id } = useParams();
+
+  const data = res?.data?.results[id];
+
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [data]);
 
   return (
-    <S.Warp>
-      <S.TitleWrap>
-        <S.Title dangerouslySetInnerHTML={{ __html: data[2] }}></S.Title>
-      </S.TitleWrap>
-      <S.ExplainWrap>
-        <div>
-          <S.CaseNum>Case-Number : {data[1]}</S.CaseNum>
-          <S.Explain dangerouslySetInnerHTML={{ __html: data[3] }}></S.Explain>
-        </div>
-        <S.Image src={data[10]} alt="nasaTech image" />
-      </S.ExplainWrap>
-    </S.Warp>
+    <>
+      {data && (
+        <S.Warp>
+          <S.TitleWrap>
+            <S.Title dangerouslySetInnerHTML={{ __html: data[2] }}></S.Title>
+          </S.TitleWrap>
+          <S.ExplainWrap>
+            <div>
+              <S.CaseNum>Case-Number : {data[1]}</S.CaseNum>
+              <S.Explain dangerouslySetInnerHTML={{ __html: data[3] }}></S.Explain>
+            </div>
+            <S.Image src={data[10]} alt="nasaTech image" />
+          </S.ExplainWrap>
+        </S.Warp>
+      )}
+    </>
   );
 }
